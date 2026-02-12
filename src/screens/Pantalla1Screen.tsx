2f0a2c1be8016a79fd464bd335a9f1aa99f65dad
soff <sof.polette@gmail.com>
2026-02-12 08:39:31 -0500

    navegacion

--- src/screens/Pantalla1Screen.tsx
+++ src/screens/Pantalla1Screen.tsx
@@ -1,10 +1,17 @@
+import { StackScreenProps } from '@react-navigation/stack';
 import React from 'react'
-import { Text, View } from 'react-native'
+import { Button, Text, View } from 'react-native'
 
-export const Pantalla1Screen = () => {
+interface Props extends StackScreenProps<any, any> { };
+
+export const Pantalla1Screen = ({navigation}: Props) => {
+    //console.log(props);
+
   return (
     <View>
         <Text>Pantalla 1 Screen</Text>
+        <Button title = 'Ir página 2'
+            onPress={()=> navigation.navigate('Pantalla 2')}/>
     </View>
   )
 }
